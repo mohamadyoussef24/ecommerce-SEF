e7053f8dfabc6405baafda1af75acc373265245b
mohamadyoussef24 <mohammadyoussef.cs@gmail.com>
2023-08-01 09:19:07 +0300

    create categories table

--- ecommerce-server/database/migrations/2023_08_01_061553_categorie.php
+++ ecommerce-server/database/migrations/2023_08_01_061553_categorie.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('details');
-            $table->string('price');
-            $table->string('image', 255);
+            $table->string('category_name');
         });
     }
 
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
